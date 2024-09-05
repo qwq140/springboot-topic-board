5a5600ca5dc0b516546b305e907ab0646f70d85b
qwq140 <qwq140@gmail.com>
2024-09-05 09:50:31 +0900

    init

---
+++ src/main/java/com/pjb/topicboard/TopicBoardApplication.java
@@ -0,0 +1,13 @@
+package com.pjb.topicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjb/topicboard/TopicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjb.topicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
