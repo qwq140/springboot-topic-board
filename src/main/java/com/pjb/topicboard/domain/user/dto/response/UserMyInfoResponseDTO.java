25653d8003ced0215e3b2e641e8035e9d9c3f994
qwq140 <qwq140@gmail.com>
2024-09-11 17:42:28 +0900

    feat : 내 정보 조회 api 응답 데이터 roles 추가

--- src/main/java/com/pjb/topicboard/domain/user/dto/response/UserMyInfoResponseDTO.java
+++ src/main/java/com/pjb/topicboard/domain/user/dto/response/UserMyInfoResponseDTO.java
@@ -1,17 +1,22 @@
 package com.pjb.topicboard.domain.user.dto.response;
 
 import com.pjb.topicboard.model.user.UserEntity;
+import com.pjb.topicboard.model.user.UserRoleType;
 import lombok.Getter;
+
+import java.util.Set;
 
 @Getter
 public class UserMyInfoResponseDTO {
     private Long id;
     private String username;
     private String nickname;
+    private Set<UserRoleType> roles;
 
     public UserMyInfoResponseDTO(UserEntity user) {
         this.id = user.getId();
         this.username = user.getUsername();
         this.nickname = user.getNickname();
+        this.roles = user.getRole();
     }
 }
